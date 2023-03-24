a64afe3a7f3fefcca54228974841fa2514d50a61
paula sirisompund <paulaclark30@yahoo.com>
2023-03-24 15:38:50 -0500

    Fixed File structure, index.html done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
-let about = $(".about");
-let banner = $(".banner");
-let callToAction = $(".call-to-action");
-
-callToAction.click( () => {
-    banner.slideToggle(500);
-    about.slideToggle(500);
+$(".call-to-action").click( () => {
+    $(".banner").slideToggle(500);
+    $(".about").slideToggle(500);
 })
+
