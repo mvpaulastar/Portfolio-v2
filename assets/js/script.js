7279feb17b643a0a435ac8f68665da8d79b387ed
paula sirisompund <paulaclark30@yahoo.com>
2023-03-24 22:08:46 -0400

    Made mobile responsive menu, added additional styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+/*Toggle banner and about section on button click*/
 $(".call-to-action").click( () => {
     $(".banner").slideToggle(500);
     $(".about").slideToggle(500);
 })
+
+// /*Responsive Mobile Menu*/
+$(".hamburger").click( () => {
+    $(".navbar").animate({left: "0px"});
+});
+$(".menu-exit").click( () => {
+    $(".navbar").animate({left: "-110%"});
+});
 
