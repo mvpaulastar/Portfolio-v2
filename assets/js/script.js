9977fb3819093a2f5361b2533c1ec5613c978bb9
paula sirisompund <paulaclark30@yahoo.com>
2023-03-25 12:40:54 -0400

    redid style file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,15 @@ $(".call-to-action").click( () => {
     $(".about").slideToggle(500);
 })
 
-// /*Responsive Mobile Menu*/
+/*Responsive Mobile Menu*/
 $(".hamburger").click( () => {
     $(".navbar").animate({left: "0px"});
 });
 $(".menu-exit").click( () => {
     $(".navbar").animate({left: "-110%"});
 });
-
+$(window).resize( () => {
+    if($(window).width() > 700){
+        $(".navbar").animate({left: "0px"})
+    }
+});
